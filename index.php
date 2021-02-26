61b5e2a4aa7da6bfb3c057b5dc44ab3a3d05a723
Harlene4 <77616751+Harlene4@users.noreply.github.com>
2021-02-26 19:00:50 +0800

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
   <head> <title> MAJOR PT </title>
   </head>
   </head>
-<frameset cols= "50%, 50%">
+<frameset cols= "50%, 50%>
                  <frame src="">
                  <frame src="https://homew3b.herokuapp.com/"
 <frameset rows= "50%, 50%">
